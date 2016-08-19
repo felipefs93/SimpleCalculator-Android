7b05907fcf33003a9dc4767dc2848f9ef6a6d20f
Felipe Soares <soaresfelipef@gmail.com>
2016-08-19 01:36:26 -0300

    Button actions were linked

--- app/src/main/java/com/soaresfelipef/simplecalculator/MainActivity.java
+++ app/src/main/java/com/soaresfelipef/simplecalculator/MainActivity.java
@@ -2,9 +2,11 @@ package com.soaresfelipef.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,37 @@ public class MainActivity extends AppCompatActivity {
         editTextNumber1 = (EditText) findViewById(R.id.editTextNumber1);
         editTextNumber2 = (EditText) findViewById(R.id.editTextNumber2);
         textViewResult = (TextView) findViewById(R.id.textView);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"The add button was clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
